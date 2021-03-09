0cf9251abdfdf1c5df6af7928091c2e8652b8a2d
Tom Moffat <tsmoffat@gmail.com>
2021-03-09 10:18:30 +0000

    updated so sar works

---
+++ blender/sar_script_ref.m
@@ -0,0 +1,174 @@
+c = physconst('LightSpeed');
+fc = 4e9;
+rangeResolution = 3;  
+crossRangeResolution = 3;
+bw = c/(2*rangeResolution);
+prf = 1000; 
+aperture = 4;  
+tpd = 3*10^-6; 
+fs = 120*10^6;
+waveform = phased.LinearFMWaveform('SampleRate',fs, 'PulseWidth', tpd, 'PRF', prf,...
+    'SweepBandwidth', bw);
+speed = 100;  
+flightDuration = 4;
+radarPlatform  = phased.Platform('InitialPosition', [0;-200;500], 'Velocity', [0; speed; 0]);
+slowTime = 1/prf;
+numpulses = flightDuration/slowTime +1;
+    
+maxRange = 2500;
+truncrangesamples = ceil((2*maxRange/c)*fs);
+fastTime = (0:1/fs:(truncrangesamples-1)/fs);
+% Set the reference range for the cross-range processing.
+Rc = 1000;
+
+antenna = phased.CosineAntennaElement('FrequencyRange', [1e9 6e9]);
+antennaGain = aperture2gain(aperture,c/fc); 
+
+transmitter = phased.Transmitter('PeakPower', 50e3, 'Gain', antennaGain);
+radiator = phased.Radiator('Sensor', antenna,'OperatingFrequency', fc, 'PropagationSpeed', c);
+
+collector = phased.Collector('Sensor', antenna, 'PropagationSpeed', c,'OperatingFrequency', fc);
+receiver = phased.ReceiverPreamp('SampleRate', fs, 'NoiseFigure', 30);
+
+channel = phased.FreeSpace('PropagationSpeed', c, 'OperatingFrequency', fc,'SampleRate', fs,...
+    'TwoWayPropagation', true);
+
+targetpos= [800,0,0;1000,0,0; 1300,0,0]'; 
+
+targetvel = [0,0,0;0,0,0; 0,0,0]';
+    
+target = phased.RadarTarget('OperatingFrequency', fc, 'MeanRCS', [1,1,1]);
+pointTargets = phased.Platform('InitialPosition', targetpos,'Velocity',targetvel);
+% The figure below describes the ground truth based on the target
+% locations.
+figure(1);h = axes;plot(targetpos(2,1),targetpos(1,1),'*g');hold all;plot(targetpos(2,2),targetpos(1,2),'*r');hold all;plot(targetpos(2,3),targetpos(1,3),'*b');hold off;
+set(h,'Ydir','reverse');xlim([-10 10]);ylim([700 1500]);
+title('Ground Truth');ylabel('Range');xlabel('Cross-Range');
+
+% Define the broadside angle
+refangle = zeros(1,size(targetpos,2));
+rxsig = zeros(truncrangesamples,numpulses);
+for ii = 1:numpulses
+    % Update radar platform and target position
+    [radarpos, radarvel] = radarPlatform(slowTime);
+    [targetpos,targetvel] = pointTargets(slowTime);
+    
+    % Get the range and angle to the point targets
+    [targetRange, targetAngle] = rangeangle(targetpos, radarpos);
+    
+    % Generate the LFM pulse
+    sig = waveform();
+    % Use only the pulse length that will cover the targets.
+    sig = sig(1:truncrangesamples);
+    
+    % Transmit the pulse
+    sig = transmitter(sig);
+    
+    % Define no tilting of beam in azimuth direction
+    targetAngle(1,:) = refangle;
+    
+    % Radiate the pulse towards the targets
+    sig = radiator(sig, targetAngle);
+    
+    % Propagate the pulse to the point targets in free space
+    sig = channel(sig, radarpos, targetpos, radarvel, targetvel);
+    
+    % Reflect the pulse off the targets
+    sig = target(sig);
+    
+    % Collect the reflected pulses at the antenna
+    sig = collector(sig, targetAngle);
+    
+    % Receive the signal  
+    rxsig(:,ii) = receiver(sig);
+    
+end
+
+imagesc(real(rxsig));title('SAR Raw Data')
+xlabel('Cross-Range Samples')
+ylabel('Range Samples')
+
+pulseCompression = phased.RangeResponse('RangeMethod', 'Matched filter', 'PropagationSpeed', c, 'SampleRate', fs);
+matchingCoeff = getMatchedFilter(waveform);
+[cdata, rnggrid] = pulseCompression(rxsig, matchingCoeff);
+
+imagesc(real(cdata));title('SAR Range Compressed Data')
+xlabel('Cross-Range Samples')
+ylabel('Range Samples')
+
+rma_processed = helperRangeMigration(cdata,fastTime,fc,fs,prf,speed,numpulses,c,Rc);
+bpa_processed = helperBackProjection(cdata,rnggrid,fastTime,fc,fs,prf,speed,crossRangeResolution,c);
+
+figure(1);
+imagesc((abs((rma_processed(1700:2300,600:1400).'))));
+title('SAR Data focused using Range Migration algorithm ')
+xlabel('Cross-Range Samples')
+ylabel('Range Samples')
+
+figure(2)
+imagesc((abs(bpa_processed(600:1400,1700:2300))));
+title('SAR Data focused using Back-Projection algorithm ')
+xlabel('Cross-Range Samples')
+ylabel('Range Samples')
+
+function azcompresseddata = helperRangeMigration(sigData,fastTime,fc,fs,prf,speed,numPulses,c,Rc)
+    frequencyRange = linspace(fc-fs/2,fc+fs/2,length(fastTime));
+    krange = 2*(2*pi*frequencyRange)/c;
+    kaz = 2*pi*linspace(-prf/2,prf/2,numPulses)./speed;
+    kazimuth = kaz.';
+    kx = krange.^2-kazimuth.^2;
+    kx = sqrt(kx.*(kx > 0));
+    kFinal = exp(1i*kx.*Rc);
+    sdata =fftshift(fft(fftshift(fft(sigData,[],1),1),[],2),2);
+    fsmPol = (sdata.').*kFinal;
+    stoltPol = fsmPol;
+    for i = 1:size((fsmPol),1)
+        stoltPol(i,:) = interp1(kx(i,:),fsmPol(i,:),krange(1,:));
+    end
+    stoltPol(isnan(stoltPol)) = 1e-30;
+    stoltPol = stoltPol.*exp(-1i*krange.*Rc);
+    azcompresseddata = ifft2(stoltPol);
+end
+
+function data = helperBackProjection(sigdata,rnggrid,fastTime,fc,fs,prf,speed,crossRangeResolution,c)
+data = zeros(size(sigdata));
+azimuthDist = -200:speed/prf:200;%azimuth distance
+rangelims = [700 1400];
+crossrangelims = [-10 10];
+rangeIdx =  [find(rnggrid>rangelims(1), 1) find(rnggrid<rangelims(2),1,'last')];
+crossrangeIdxStart = find(azimuthDist>crossrangelims(1),1);
+crossrangeIdxStop = find(azimuthDist<crossrangelims(2),1,'last');
+for i= rangeIdx(1):rangeIdx(2)  % Iterate over the range indices
+    
+    % Using desired cross-range resolution, compute the synthetic aperture
+    % length
+    lsynth= (c/fc)* (c*fastTime(i)/2)/(2*crossRangeResolution);
+    lsar = round(lsynth*length(azimuthDist)/azimuthDist(end)) ;
+    % Ensure lsar is an odd number
+    lsar = lsar + mod(lsar,2);    
+    
+    % Construct hanning window for cross-range processing, to suppress the
+    % azimuthal side lobes
+    hn= hanning(lsar).';
+    % Iterate over the cross-range indices
+    for j= crossrangeIdxStart:crossrangeIdxStop 
+        % azimuth distance in x direction over cross-range indices
+        posx= azimuthDist(j);
+        % range in y-direction over range indices
+        posy= c*fastTime(i)/2;
+        % initializing count to zero
+        count= 0;
+        % Iterate over the synthetic aperture
+        for k= j-lsar/2 +1:j+ lsar/2 
+            % Time delay for each of range and cross-range indices
+            td= sqrt((azimuthDist(k)- posx)^2 + posy^2)*2/c;
+            cell= round(td*fs) +1 ;
+            signal = sigdata(cell,k);
+            count= count + hn(k -(j-lsar/2))*signal *exp(1j*2*pi*fc*(td));
+        end
+        % Processed data at each of range and cross-range indices
+        data(i,j)= count;
+    end
+    
+end
+end
