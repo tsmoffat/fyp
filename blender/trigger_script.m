31c69d83b023b26a8c9f959a2832840a7e58ee14
Tom Moffat <tsmoffat@gmail.com>
2021-04-14 09:06:43 +0100

    finally got the project working again, plus report updates

--- blender/trigger_script.m
+++ blender/trigger_script.m
@@ -11,37 +11,64 @@ reference_range = 800;
 
 sim_out = sim(model, 'SimulationMode', simMode);
 
-figure(3)
-imagesc(real(reshape(sim_out.range_data, [], 4001))); title('SAR Raw Data')
+figure(1)
+imagesc(real(reshape(sim_out.spotlight_data, [], 4001))); title('SAR Raw Spotlight Mode Data')
+xlabel('Cross-range')
+ylabel('Down-range')
+
+figure(2)
+imagesc(real(reshape(sim_out.stripmap_data, [], 4001))); title('SAR Raw Stripmap Mode Data')
 xlabel('Cross-range')
 ylabel('Down-range')
 
 matched_filter_coeffs = sim_out.lin_fm_coeffs(1:360);
 
 pulseCompression = phased.RangeResponse('RangeMethod', 'Matched filter', 'PropagationSpeed', c, 'SampleRate', fs);
-[cdata, range_grid] = pulseCompression(sim_out.range_data, matched_filter_coeffs);
+[spotlight_data, spotlight_range_grid] = pulseCompression(sim_out.spotlight_data, matched_filter_coeffs);
+[stripmap_data, stripmap_range_grid] = pulseCompression(sim_out.stripmap_data, matched_filter_coeffs);
 
-reshaped_data = reshape(cdata, 2002, []);
-reshaped_range_grid = reshape(range_grid, 2002, []);
+spotlight_reshaped_data = reshape(spotlight_data, 2002, []);
+reshaped_spotlight_range_grid = reshape(spotlight_range_grid, 2002, []);
+stripmap_reshaped_data = reshape(spotlight_data, 2002, []);
+reshaped_stripmap_range_grid = reshape(spotlight_range_grid, 2002, []);
 
-figure(4)
-imagesc(real(reshaped_data)); title('SAR Range Compressed Data')
+figure(3)
+imagesc(real(spotlight_reshaped_data)); title('SAR Range Compressed Data - Spotlight')
 xlabel('Cross-range')
 ylabel('Down-range')
 
-omega_k_image = omegak(reshaped_data, fs, maxRange, fc, flightTime, speed, reference_range, prf);
+figure(4)
+imagesc(real(stripmap_reshaped_data)); title('SAR Range Compressed Data - Stripmap')
+xlabel('Cross-range')
+ylabel('Down-range')
 
-figure(1)
-imagesc((abs(omega_k_image.')));
-title('SAR Data focused using Omega-K algorithm')
+omega_k_image_spotlight = omegak(spotlight_reshaped_data, fs, maxRange, fc, flightTime, speed, reference_range, prf);
+omega_k_image_stripmap = omegak(stripmap_reshaped_data, fs, maxRange, fc, flightTime, speed, reference_range, prf);
+
+figure(5)
+imagesc((abs(omega_k_image_spotlight.')));
+title('Spotlight SAR Data focused using Omega-K algorithm')
 xlabel('Cross-Range Samples')
 ylabel('Range Samples')
 
-back_project_image = helperBackProjection(reshaped_data, reshaped_range_grid, fastTime, fc, fs, speed, crossRangeResolution, c, prf);
+figure(6)
+imagesc((abs(omega_k_image_stripmap.')));
+title('Stripmap SAR Data focused using Omega-K algorithm')
+xlabel('Cross-Range Samples')
+ylabel('Range Samples')
 
-figure(2)
-imagesc((abs(back_project_image)))
-title('SAR Data focused using back projection algorithm')
+backproject_image_spotlight = helperBackProjection(spotlight_reshaped_data, reshaped_spotlight_range_grid, fastTime, fc, fs, speed, crossRangeResolution, c, prf);
+backproject_image_stripmap = helperBackProjection(stripmap_reshaped_data, reshaped_stripmap_range_grid, fastTime, fc, fs, speed, crossRangeResolution, c, prf);
+
+figure(7)
+imagesc((abs(backproject_image_spotlight)))
+title('Spotlight SAR Data focused using back projection algorithm')
+xlabel('Cross-Range Samples')
+ylabel('Range Samples')
+
+figure(8)
+imagesc((abs(backproject_image_stripmap)))
+title('Spotlight SAR Data focused using back projection algorithm')
 xlabel('Cross-Range Samples')
 ylabel('Range Samples')
 
